9b334947f3a88b8a2ea7d5cb5d2655a746579076
carreterosanti <santiago.carretero@outlook.com>
2020-08-19 00:33:04 -0300

    TopHeader 'Container'

---
+++ src/views/top-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="top-header">
+    <b-navbar toggleable="lg" type="dark">
+      <b-navbar-brand href="#"
+        ><img class="top-header__logo" src="../assets/logo-small-light.png"
+      /></b-navbar-brand>
+
+      <b-navbar-nav>
+        <b-nav-item href="#">File</b-nav-item>
+        <b-nav-item href="#">Edit</b-nav-item>
+        <b-nav-item href="#">Selection</b-nav-item>
+        <b-nav-item href="#">View</b-nav-item>
+        <b-nav-item href="#">Help</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script src="./top-header.js"></script>
+<style src="./top-header.scss" lang="scss"></style>
