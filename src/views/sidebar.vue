0e73e4602f25f844fcbc8f813caad2d38dc8f036
carreterosanti <santiago.carretero@outlook.com>
2020-08-19 00:33:23 -0300

    Sidebar 'Container'

---
+++ src/views/sidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="sidebar">
+    <font-awesome-icon class="sidebar__icon" :icon="['fab', 'angellist']" />
+    <font-awesome-icon class="sidebar__icon" :icon="['fas', 'cog']" />
+    <font-awesome-icon class="sidebar__icon" :icon="['fas', 'chart-bar']" />
+    <font-awesome-icon class="sidebar__icon" :icon="['fas', 'life-ring']" />
+  </div>
+</template>
+
+<script src="./sidebar.js"></script>
+<style src="./sidebar.scss" lang="scss"></style>
