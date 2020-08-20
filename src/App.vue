f9fad4359dc45b0a0ead456a1d6127b6ff04d540
carreterosanti <santiago.carretero@outlook.com>
2020-08-20 20:12:45 -0300

    Working merge with develop

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <MainScene />
+    <!-- <MainScene /> -->
+    <main-container />
   </div>
 </template>
 
 <script>
-import MainScene from "./components/MainScene.vue";
+// import MainScene from "./components/MainScene.vue";
+import MainContainer from "./views/main-container.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 export default {
   name: "App",
   components: {
-    MainScene
-  }
+    // MainScene
+    MainContainer,
+  },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
