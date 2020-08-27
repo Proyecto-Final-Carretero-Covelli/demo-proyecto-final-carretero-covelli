b1cb911101bf55bcdbef8469a9e64d1256042b5e
carreterosanti <santiago.carretero@outlook.com>
2020-08-26 22:48:41 -0300

    Avances sobre array.vue

--- src/components/konva/array/array.vue
+++ src/components/konva/array/array.vue
@@ -1,11 +1,11 @@
 <template>
   <v-group :config="configArrayGroup">
     <v-text :config="configArrayName"></v-text>
-    <template v-for="(arrayItem,i) in arrayItems">
+    <!-- <template v-for="(arrayItem,i) in arrayItems">
       <v-rect :config="configArrayRect"></v-rect>
-      <!-- Implementar un metodo que retorne el obj de configuración dados los parametros dados -->
-      <v-text :config="configArrayValue"></v-text>
-    </template>
+    Implementar un metodo que retorne el obj de configuración dados los parametros dados-->
+    <!-- <v-text :config="configArrayValue"></v-text>
+    </template>-->
   </v-group>
 </template>
 
