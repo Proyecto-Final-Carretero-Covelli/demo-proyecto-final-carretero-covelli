6a52f5e8f3a8aeda29d8d77220ac75df099192cc
carreterosanti <santiago.carretero@outlook.com>
2020-08-31 20:33:33 -0300

    Fix actualización variables Konva, configs declaradas como variables computadas en vez de 'data inicializada en el mounted'

--- src/components/konva/variable/variable.js
+++ src/components/konva/variable/variable.js
@@ -6,40 +6,46 @@ export default {
     return {
       elementStartX: 0,
       configVariableGroup: { draggable: true },
-      configVariableName: {},
-      configVariableRect: {},
-      configVariableValue: {},
     };
+  },
+
+  computed: {
+    configVariableName: function() {
+      return {
+        x: this.elementStartX,
+        y: 70,
+        text: this.name,
+        width: sizeRect,
+        height: sizeRect,
+        align: "center",
+        verticalAlig: "middle",
+        fill: "white",
+      };
+    },
+    configVariableValue: function() {
+      return {
+        x: this.elementStartX,
+        y: 100,
+        text: this.value,
+        width: sizeRect,
+        height: sizeRect,
+        align: "center",
+        verticalAlign: "middle",
+        fill: "white",
+      };
+    },
+    configVariableRect: function() {
+      return {
+        x: this.elementStartX,
+        y: 100,
+        width: sizeRect,
+        height: sizeRect,
+        fill: "#3b6978",
+      };
+    },
   },
 
   mounted() {
     this.elementStartX = this.elementIndex() * 100;
-    this.configVariableName = {
-      x: this.elementStartX,
-      y: 70,
-      text: this.name,
-      width: sizeRect,
-      height: sizeRect,
-      align: "center",
-      verticalAlig: "middle",
-      fill: "white",
-    };
-    this.configVariableRect = {
-      x: this.elementStartX,
-      y: 100,
-      width: sizeRect,
-      height: sizeRect,
-      fill: "#3b6978",
-    };
-    this.configVariableValue = {
-      x: this.elementStartX,
-      y: 100,
-      text: this.value,
-      width: sizeRect,
-      height: sizeRect,
-      align: "center",
-      verticalAlign: "middle",
-      fill: "white",
-    };
   },
 };
