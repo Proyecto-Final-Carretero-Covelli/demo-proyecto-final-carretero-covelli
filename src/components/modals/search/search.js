5d240a2b6375fc932440947f3d8798a073aeddb4
carreterosanti <santiago.carretero@outlook.com>
2020-10-19 21:12:32 -0300

    Control al seleccionar una carpeta para que no se ejecute como selección de un ejercicio

--- src/components/modals/search/search.js
+++ src/components/modals/search/search.js
@@ -24,16 +24,18 @@ export default {
     },
 
     selectExercise(selectedExercise) {
-      let exercise = selectedExercise.exercise;
-      Object.keys(exercise.suiteTest).forEach((testId) => {
-        this._setTestFlagIconInfo(exercise.suiteTest[testId], exercise);
-      });
+      if (!selectedExercise.children) {
+        let exercise = selectedExercise.exercise;
+        Object.keys(exercise.suiteTest).forEach((testId) => {
+          this._setTestFlagIconInfo(exercise.suiteTest[testId], exercise);
+        });
 
-      this.$store.commit("setCurrentExercise", exercise);
-      this.$store.commit("setTitle", exercise.name);
-      this.$store.commit("setTitleText", exercise.statement);
+        this.$store.commit("setCurrentExercise", exercise);
+        this.$store.commit("setTitle", exercise.name);
+        this.$store.commit("setTitleText", exercise.statement);
 
-      this.$bvModal.hide("modal-search");
+        this.$bvModal.hide("modal-search");
+      }
     },
 
     _setTestFlagIconInfo(test, exercise) {
