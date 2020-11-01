56e7c6496ac3c6e9e774f6978c3f4f90474c000f
carreterosanti <santiago.carretero@outlook.com>
2020-11-01 20:26:45 -0300

    Dropdown 'Cerrar Ejercicio'

--- src/views/top-header.vue
+++ src/views/top-header.vue
@@ -2,6 +2,16 @@
   <div class="top-header">
     <div class="w-25">
       <img class="top-header__logo" src="../assets/logo-small-light.png" />
+      <b-dropdown
+        v-if="$store.getters.getTitle !== null"
+        id="dropdown-1"
+        text="Archivo"
+        class="m-md-2 btn-drop-down"
+      >
+        <b-dropdown-item @click="closeExercise"
+          >Cerrar Ejercicio</b-dropdown-item
+        >
+      </b-dropdown>
     </div>
 
     <div class="top-header__title w-50">
@@ -11,13 +21,19 @@
       >
         <p>{{ $store.getters.getTitle }}</p>
         <font-awesome-icon
-          v-if="$store.getters.getResizeTitle && $store.getters.getResizeTitle.percent <= 1"
+          v-if="
+            $store.getters.getResizeTitle &&
+              $store.getters.getResizeTitle.percent <= 1
+          "
           class="top-header__controll-icon top-header__title--shows"
           :icon="['fas', 'angle-down']"
           @click="toggleTitleShow"
         />
         <font-awesome-icon
-          v-if="$store.getters.getResizeTitle && $store.getters.getResizeTitle.percent > 1"
+          v-if="
+            $store.getters.getResizeTitle &&
+              $store.getters.getResizeTitle.percent > 1
+          "
           class="top-header__controll-icon top-header__title--shows"
           :icon="['fas', 'angle-up']"
           @click="toggleTitleShow"
@@ -26,12 +42,19 @@
     </div>
 
     <div class="top-header__controll w-25">
-
-      <div v-if="$store.getters.isRunningCode" class="top-header__controll-icon">
+      <div
+        v-if="$store.getters.isRunningCode"
+        class="top-header__controll-icon"
+      >
         <span> Cargando... </span>
-        <b-spinner small type="grow" label="Loading..." class="top-header__controll-icon"></b-spinner>
+        <b-spinner
+          small
+          type="grow"
+          label="Loading..."
+          class="top-header__controll-icon"
+        ></b-spinner>
       </div>
-      
+
       <font-awesome-icon
         class="top-header__controll-icon top-header__controll-icon--play"
         :icon="['fas', 'play']"
@@ -39,18 +62,30 @@
       />
 
       <div v-if="$store.getters.isDebugging">
-        <font-awesome-icon class="top-header__controll-icon" :icon="['fas', 'arrow-right']" @click="$store.dispatch('nextStep')"/>
+        <font-awesome-icon
+          class="top-header__controll-icon"
+          :icon="['fas', 'arrow-right']"
+          @click="$store.dispatch('nextStep')"
+        />
         <font-awesome-icon
           class="top-header__controll-icon top-header__controll-icon--stop"
-          :icon="['fas', 'stop']" @click="$store.dispatch('stop')"
+          :icon="['fas', 'stop']"
+          @click="$store.dispatch('stop')"
         />
       </div>
 
       <div v-else>
-        <font-awesome-icon class="top-header__controll-icon" :icon="['fas', 'clock']" @click="$store.dispatch('runInSlowMode')" />
-        <font-awesome-icon class="top-header__controll-icon" :icon="['fas', 'bug']" @click="$store.dispatch('runInDebugMode')" />
+        <font-awesome-icon
+          class="top-header__controll-icon"
+          :icon="['fas', 'clock']"
+          @click="$store.dispatch('runInSlowMode')"
+        />
+        <font-awesome-icon
+          class="top-header__controll-icon"
+          :icon="['fas', 'bug']"
+          @click="$store.dispatch('runInDebugMode')"
+        />
       </div>
-      
     </div>
   </div>
 </template>
